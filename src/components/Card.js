997eec394cb329b1dc2b90062adfae2feef31b2c
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2022-03-13 12:44:08 +0500

    👌 IMPROVE: Add click handling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import "./Card.css";
 
 const Card = ({ card, handleChoice }) => {
+    const handleClick = () => {
+        handleChoice(card);
+    };
+
     return (
         <div className="card">
             <div>
                 <img className="front" src={card.src} alt="Card Front" />
                 <img
                     className="back"
-                    onClick={handleChoice(card)}
+                    onClick={handleClick}
                     src="/images/cover.png"
                     alt="Card Back"
                 />
